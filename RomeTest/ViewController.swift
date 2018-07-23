63a77d48f17d321534e2c9c4402705feee6385d9
Renzo Crisóstomo <renzo.crisostomomedina@here.com>
2018-07-23 12:41:58 +0200

    Initial commit

---
+++ RomeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RomeTest
+//
+//  Created by Crisostomo Medina, Renzo on 23.07.18.
+//  Copyright © 2018 Crisostomo Medina, Renzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ RomeTestTests/RomeTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  RomeTestTests.swift
+//  RomeTestTests
+//
+//  Created by Crisostomo Medina, Renzo on 23.07.18.
+//  Copyright © 2018 Crisostomo Medina, Renzo. All rights reserved.
+//
+
+import XCTest
+@testable import RomeTest
+
+class RomeTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
